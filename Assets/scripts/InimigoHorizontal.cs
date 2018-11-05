4447ace31180e9b8704a55185b57ba09e6faae03
josimar <josimarfiuzamelo@hotmail.com>
2018-11-05 04:37:46 -0200

    add

---
+++ Assets/scripts/InimigoHorizontal.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InimigoHorizontal : MonoBehaviour {
+
+    private bool colide = false;
+    private float move = -2;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move, GetComponent<Rigidbody2D>().velocity.y);
+        if(colide){
+            Flip();
+        }
+	}
+
+    private void Flip(){
+        move *= -1;
+        GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+        colide = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Plataformas")){
+            colide = true;
+
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Plataformas"))
+        {
+            colide = false;
+
+        }
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+	public float forcaPulo;
+	public float velocidadeMaxima;
+
+	public int lives;
+	public int rings;
+
+	public Text TextLives;
+	public Text TextRings;
+
+	public bool isGrounded;
+	
+	void Start ()
+	{
+		TextLives.text = lives.ToString();
+		TextRings.text = rings.ToString();
+	}
+	
+	void Update ()
+	{
+		Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+		
+		float movimento = Input.GetAxis("Horizontal");
+		
+		rigidbody.velocity = new Vector2(movimento*velocidadeMaxima,rigidbody.velocity.y);
+
+		if (movimento < 0)
+		{
+			GetComponent<SpriteRenderer>().flipX = true;
+		}else if (movimento > 0)
+		{
+			GetComponent<SpriteRenderer>().flipX = false;
+		}
+
+
+		if (movimento > 0 || movimento < 0)
+		{
+			GetComponent<Animator>().SetBool("walking", true);
+		}
+		else
+		{
+			GetComponent<Animator>().SetBool("walking", false);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+		{
+			rigidbody.AddForce(new Vector2(0,forcaPulo));
+			GetComponent<AudioSource>().Play();
+		}
+
+		if (isGrounded)
+		{
+			GetComponent<Animator>().SetBool("jumping", false);
+		}
+		else
+		{
+			GetComponent<Animator>().SetBool("jumping", true);
+		}
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision2D)
+    {
+        if (collision2D.gameObject.CompareTag("Moedas"))
+        {
+            Destroy(collision2D.gameObject);
+            rings++;
+            TextRings.text = rings.ToString();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2D)
+	{
+		if ( collision2D.gameObject.CompareTag("Monstros"))
+		{
+			//Criar lógica para perder vida
+		}
+		
+		if ( collision2D.gameObject.CompareTag("Plataformas"))
+		{
+			isGrounded = true;
+		}
+		
+	}
+
+	
+	void OnCollisionExit2D(Collision2D collision2D)
+	{
+		if ( collision2D.gameObject.CompareTag("Plataformas"))
+		{
+			isGrounded = false;
+		}
+	}
+}
